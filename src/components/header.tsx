56ab492de9414866ff75717336defc04d137b192
Yarik <coffe1n21@gmail.com>
2025-06-24 15:49:07 +0700

    db download

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Navigation from "./navigation-button";
 import UserAuth from "@/components/auth/user-auth-navigation";
 import ToggleThemeButton from "./tiggleThemeButton";
+import { FileDown } from "lucide-react";
 
 export default function Header() {
   const articlesNavigation = [
@@ -22,9 +23,16 @@ export default function Header() {
       </div>
       <ToggleThemeButton />
       <nav className="w-full flex justify-between">
-        <div className="flex">
+        <div className="flex items-center gap-3">
           <Navigation title="Статьи" elements={articlesNavigation} />
           <Navigation title="Играть" elements={chessNavigation} />
+          <a
+            href="/chess_openings.db"
+            className="text-neutral-400 flex flex-row gap-1 hover:underline"
+          >
+            База дебютов
+            <FileDown strokeWidth={1} />
+          </a>
         </div>
 
         {/* User Login */}
